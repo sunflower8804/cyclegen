d9dd3f54cfebcce2126977ba62e3e45e3cf22945
Jay <jayolesonbz@gmail.com>
2024-08-19 11:59:55 -0500

    ooops

--- scripts/clan.py
+++ scripts/clan.py
@@ -301,29 +301,12 @@ class Clan:
         game.save_clanlist(self.name)
         game.switches["clan_list"] = game.read_clans()
 
-<<<<<<< HEAD
-=======
-        # this must be done after saving the first time!
->>>>>>> 2c986d68d0386d4e9059305f2b5a9a70d0550664
-        if self.clan_age == "established":
-            self.generate_mates()
-            self.generate_families()
-            self.populate_sc()
-            self.populate_ur()
-            self.populate_df()
-<<<<<<< HEAD
-
-=======
-        game.save_cats()
-        self.save_clan()
-
         # this has to be done after saving the first time
         # doing this without any previous clans will cause a crash otherwise
         if self.clan_age == "established":
             self.populate_sc()
             self.populate_ur()
             self.populate_df()
->>>>>>> 2c986d68d0386d4e9059305f2b5a9a70d0550664
         game.save_cats()
         self.save_clan()
 
