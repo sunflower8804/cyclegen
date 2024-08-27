6272058de9f0236d652d2e2c0f343329b09768a4
Jay <jayolesonbz@gmail.com>
2024-08-27 18:21:56 -0500

    accessory > accessories fixes

--- scripts/events_module/relationship/pregnancy_events.py
+++ scripts/events_module/relationship/pregnancy_events.py
@@ -856,7 +856,10 @@ class Pregnancy_Events:
                     kit.relationships[the_cat.ID] = Relationship(kit, the_cat)
 
             #### REMOVE ACCESSORY ######
-            kit.pelt.accessory = None
+            # kit.pelt.accessory = None
+            kit.pelt.accessories = []
+            kit.pelt.inventory = []
+            
             clan.add_cat(kit)
 
             #### GIVE HISTORY ######
