d2313d98a5c7390fe7ac111cb0f393ab66699878
Jay <144476842+jaysoleson@users.noreply.github.com>
2024-11-02 17:08:24 -0500

    Update TalkScreen.py

--- scripts/screens/TalkScreen.py
+++ scripts/screens/TalkScreen.py
@@ -467,13 +467,13 @@ class TalkScreen(Screens):
 
                     if special_date:
                         with open(f"{resource_dir}focuses/{special_date.patrol_tag}.json", 'r') as read_file:
-                            possible_texts2 = ujson.loads(read_file.read())
-                            possible_texts.update(possible_texts2)
+                            special_dialogue = ujson.loads(read_file.read())
+                            possible_texts.update(special_dialogue)
                             
                     if game.config['fun']['april_fools']:
                         with open(f"{resource_dir}focuses/aprilfools.json", 'r') as read_file:
-                            possible_texts2 = ujson.loads(read_file.read())
-                            possible_texts.update(possible_texts2)
+                            aprilfools_dialogue = ujson.loads(read_file.read())
+                            possible_texts.update(aprilfools_dialogue)
 
                     
         return self.filter_texts(cat, possible_texts)
