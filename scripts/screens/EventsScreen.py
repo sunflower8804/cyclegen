4c3f0245b1f41e325d2b3bae07e8a6e3546adc06
J Gynn <48025294+j-gynn@users.noreply.github.com>
2024-12-26 01:30:53 +0000

    moved some colours to config to expose them for modders

--- scripts/screens/EventsScreen.py
+++ scripts/screens/EventsScreen.py
@@ -629,9 +629,9 @@ class EventsScreen(Screens):
             )
             if i % 2 == 0:
                 self.event_display_elements[f"container{i}"].background_colour = (
-                    pygame.Color(87, 76, 55)
+                    game.config["theme"]["dark_mode_event_alternate"]
                     if game.settings["dark mode"]
-                    else pygame.Color(167, 148, 111)
+                    else game.config["theme"]["light_mode_event_alternate"]
                 )
                 self.event_display_elements[f"container{i}"].rebuild()
 
